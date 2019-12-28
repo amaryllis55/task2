ebd3a507c68386c195cf35e3519d38479ad266e0
amaryllis55 <56350788+amaryllis55@users.noreply.github.com>
2019-12-28 19:05:32 +0100

    edit

--- command.py
+++ command.py
@@ -9,12 +9,26 @@ class Connect:
 
     def close(self):
         self.client.close()
+    def show_statistics(self):
+        stats = ["position", "clealiness", "quality/price", "service", "average vote"]
+        not_selected=True
+        while(not_selected):
+            for item in options:
+                print(item + "\n")
+            print("Select an option:\n")
+            chosen = input()
+            if chosen not in options:
+                print("The selected option is not valid. Please try again: \n")
+            else:
+                not_selected=False
+        # chosen contiene l'attributo rispetto a cui fare statistica
+    def make_statistic(self, chosen):
+        print("TODO")
 
 
 if __name__ == '__main__':
 
     options = ["login", "register", "read analytics", "read statistics"]
-    stats=["position", "clealiness", "quality/price",  "service", "average vote" ]
 
     print("Options:\n")
     for item in options:
