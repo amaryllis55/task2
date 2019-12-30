a5263a6262f9399e588a2c3639e6c7005ec9a371
amaryllis55 <56350788+amaryllis55@users.noreply.github.com>
2019-12-30 16:03:53 +0100

    removed forking

--- command.py
+++ command.py
@@ -25,22 +25,29 @@ class Connect:
     def close(self):
         self.client.close()
 
+    def manageAnalytics(self):
+        opt = ["averageRating", "serviceRating", "cleanlinessRating", "positionRating"]
+        for item in opt:
+            print(item)
+        chosen = input("Select evaluation attribute:\n")
+
+
     def manageStatistics(self):
         opt=["averageRating", "serviceRating","cleanlinessRating", "positionRating"]
         for item in opt:
             print(item)
         chosen=input("Select evaluation attribute:\n")
         if chosen in opt:
-            plt = input("Select city or nation:")
+            plt = input("Select city or nation:\n")
             if plt == "city":
                 for item in self.cities:
-                    print(item )
+                    print(item[0] )
                 city = input("Select city:\n")
                 if city in self.dictionary.keys():
                     self.computeAvg(chosen, self.dictionary[city], "CityID")
             elif plt=="nation":
                 for item in self.nations:
-                    print(item)
+                    print(item[0])
                 nation = input("Select city:\n")
                 if nation in self.dictionary.keys():
                     self.computeAvg(chosen, self.dictionary[nation], "NationID")
@@ -51,9 +58,10 @@ class Connect:
     def computeAvg(self, chosen, place, type):
         db = self.client.test_database
         coll = db.hotels
-        numPos = db.hotels.count({"$and": [{chosen: {"$gt": 6}}, {type: place}]})
-        numNeg = db.hotels.count({"$and": [{chosen: {"$lt": 5}}, {type: place}]})
-        numMed = db.hotels.count({type: place}) - (numPos + numNeg)
+        numPos = db.hotels.count_documents({"$and": [{chosen: {"$gt": 6}}, {type: place}]})
+        numNeg = db.hotels.count_documents({"$and": [{chosen: {"$lt": 5}}, {type: place}]})
+        numMed = db.hotels.count_documents({type: place}) - (numPos + numNeg)
+        print(numPos, numNeg, numMed)
         #plot?
 
 
@@ -68,20 +76,21 @@ if __name__ == '__main__':
 
     while (True):
         chosen=input()
-        pid = os.fork()
-        if pid == 0:  # child process
+        #pid = os.fork()
+        #if pid == 0:  # child process
+        if chosen == options[0]:  # login
+            mongodb = Connect()
+            mongodb.close()
+        if chosen == options[1]:  # register
+            mongodb = Connect()
+            mongodb.close()
+        if chosen == options[2]:  # analitycs
+            mongodb = Connect()
+            mongodb.close()
+        if chosen == options[3]:  # statistics
+            mongodb = Connect()
+            mongodb.manageStatistics()
+            mongodb.close()
+        print("Select an option:\n")
 
-            if chosen == options[0]:  # login
-                mongodb = Connect()
-                mongodb.close()
-            if chosen == options[1]:  # register
-                mongodb = Connect()
-                mongodb.close()
-            if chosen == options[2]:  # analitycs
-                mongodb = Connect()
-                mongodb.close()
-            if chosen == options[3]:  # statistics
-                mongodb = Connect()
-                mongodb.manageStatistics()
-                mongodb.close()
 
